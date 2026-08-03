00d60bf6140dbb4cbfb75005caad20f7022c9d09
fenil viroja <fenilviroja497@gmail.com>
2026-08-03 18:52:24 +0530

    added paragraph

---
+++ addition(2).py
@@ -0,0 +1,9 @@
+import num as n
+n1=int(input("enter first no:"))
+n2=int(input("enter second no:"))
+a=n.addnum(n1,n2)
+print(a)
+a=n.subnum(n1,n2)
+print(a)
+a=n.mulnum(n1,n2)
+print(a)
---
+++ built.py
@@ -0,0 +1,4 @@
+from math import sqrt
+n1=int(input("enter first no:"))
+a=sqrt(n1)
+print(a)
