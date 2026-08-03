9601439d1fef0617e0b7ab083a0f32d3dba4e1ff
fenil viroja <fenilviroja497@gmail.com>
2026-08-03 18:41:12 +0530

    first commit

---
+++ additon.py
@@ -0,0 +1,9 @@
+import num
+n1=int(input("enter first no:"))
+n2=int(input("enter second no:"))
+a=num.addnum(n1,n2)
+print(a)
+a=num.subnum(n1,n2)
+print(a)
+a=num.mulnum(n1,n2)
+print(a)
---
+++ num.py
@@ -0,0 +1,6 @@
+def addnum(n1,n2):
+    return(n1+n2)
+def subnum(n1,n2):
+    return(n1-n2)
+def mulnum(n1,n2):
+    return(n1*n2)
