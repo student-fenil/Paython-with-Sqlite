68fa5736f1e6e17bc51c9b68dfb9a3ebe561cc64
fenil viroja <fenilviroja497@gmail.com>
2026-08-06 19:33:09 +0530

    added paragraph

---
+++ package.py/p4_package.py
@@ -0,0 +1,6 @@
+import package.first
+import package.second
+print(package.first.sum(10,20))
+print(package.first.mul(10,20))
+print(package.second.sum(4,4))
+print(package.second.mul(4,4))
---
+++ package.py/second.py
@@ -0,0 +1,6 @@
+def sum(a,b):
+    print("function of first module")
+    return a+b;
+def mul(a,b):
+    print("function of first module")
+    return a*b;
