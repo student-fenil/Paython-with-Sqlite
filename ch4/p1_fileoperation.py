e18026d0b7713324d002ffebc8ed76afce97034c
fenil viroja <fenilviroja497@gmail.com>
2026-08-21 21:03:55 +0530

    added paragraph

--- ch4/p1_fileoperation.py
+++ ch4/p1_fileoperation.py
@@ -1,12 +1,15 @@
-f1=open("temp.txt","r")
-r=f1.read()
+f1=open("temp.txt", "r")
+r = f1.read()
 print(r)
+f1.close()
 
-w=input("enter the string")
-f1=open("temp.txt","w")
+w = input("Enter the string: ")
+
+f1 = open("temp.txt", "w")
 f1.write(w)
 f1.close()
 
-f1=open("temp.txt","r")
-r=f1.read()
+f1 = open("temp.txt", "r")
+r = f1.read()
 print(r)
+f1.close()
